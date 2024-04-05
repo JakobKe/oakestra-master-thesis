99813eb84917842d362fec3c5f7defc6bb6d5443
JakobKe <jakobkempter@gmail.com>
2024-04-05 12:33:42 +0200

    Fix Format

--- root_orchestrator/system-manager-python/proto/clusterRegistration_pb2.py
+++ root_orchestrator/system-manager-python/proto/clusterRegistration_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "clusterRegistration_pb2", _globals)
-if _descriptor._USE_C_DESCRIPTORS == False:
+if _descriptor._USE_C_DESCRIPTORS is False:
     DESCRIPTOR._options = None
     _globals["_CS1MESSAGE"]._serialized_start = 50
     _globals["_CS1MESSAGE"]._serialized_end = 93
